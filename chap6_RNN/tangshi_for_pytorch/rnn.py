39577b185d969b7a99e3bee4a5235155a1d71e3f
TJU-YanJinghao <145900004+TJU-YanJinghao@users.noreply.github.com>
2025-03-21 22:45:13 +0800

    chap6 completed

--- chap6_RNN/tangshi_for_pytorch/rnn.py
+++ chap6_RNN/tangshi_for_pytorch/rnn.py
@@ -45,6 +45,7 @@ class RNN_model(nn.Module):
         # here you need to define the "self.rnn_lstm"  the input size is "embedding_dim" and the output size is "lstm_hidden_dim"
         # the lstm should have two layers, and the  input and output tensors are provided as (batch, seq, feature)
         # ???
+        self.rnn_lstm = nn.LSTM(embedding_dim,lstm_hidden_dim,num_layers=2,batch_first=True)
 
 
 
@@ -61,6 +62,9 @@ class RNN_model(nn.Module):
         # here you need to put the "batch_input"  input the self.lstm which is defined before.
         # the hidden output should be named as output, the initial hidden state and cell state set to zero.
         # ???
+        hidden = Variable(torch.zeros(2,batch_input.size(0),self.lstm_dim, device=batch_input.device))
+        cell = Variable(torch.zeros(2,batch_input.size(0),self.lstm_dim, device=batch_input.device))
+        output, _ = self.rnn_lstm(batch_input,(hidden,cell))
 
 
 
